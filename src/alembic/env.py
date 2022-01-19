afedca45fb22be2939704220b0f1c50c1b151453
Egehan Gundogdu <egehn.gundogdu@gmail.com>
2022-01-20 00:03:46 +0300

    alembic env.py file changed for mypy.

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -12,7 +12,7 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+fileConfig(config.config_file_name) # type: ignore
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -20,7 +20,7 @@ fileConfig(config.config_file_name)
 # target_metadata = mymodel.Base.metadata
 from app.database.base import Base
 
-target_metadata = Base.metadata
+target_metadata = Base.metadata # type: ignore
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
